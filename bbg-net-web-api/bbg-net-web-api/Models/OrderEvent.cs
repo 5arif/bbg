5a0d4f7c8a1ef5d4d4c0f990f2cc765b818dec75
Yosi Oliver <yosi.oliver@qualysoft-coe.com>
2026-01-02 15:56:37 +0700

    add orderevent topic

---
+++ bbg-net-web-api/bbg-net-web-api/Models/OrderEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace bbg_net_web_api.Models
+{
+    public class OrderEvent
+    {
+        public string OrderId { get; set; }
+        public double Amount { get; set; }
+        public string Customer { get; set; }
+    }
+}
